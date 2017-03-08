5ee568da5d25139aed5e9a3487a546a7e0e15e2e
wesleysliao <wesleysliao@gmail.com>
2017-03-07 21:06:56 -0600

    init commit with tivaware and lm4flash

---
+++ TivaC/TivaWare/examples/boards/ek-tm4c123gxl/blinky/gcc/blinky.d
@@ -0,0 +1,10 @@
+gcc/blinky.o: blinky.c /usr/lib/gcc/arm-none-eabi/6.2.1/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/_newlib_version.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h \
+ /usr/lib/gcc/arm-none-eabi/6.2.1/include/stdbool.h \
+ ../../../../inc/hw_memmap.h ../../../../driverlib/debug.h \
+ ../../../../driverlib/gpio.h ../../../../driverlib/sysctl.h
