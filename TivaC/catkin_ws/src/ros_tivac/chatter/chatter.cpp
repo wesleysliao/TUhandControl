b1c5160e21ff87c8a89bf57c75e823d0a7f68f45
wesleysliao <wesleysliao@gmail.com>
2017-03-24 16:58:49 -0500

    Added #defines for all the pins used so far.

--- TivaC/catkin_ws/src/ros_tivac/chatter/chatter.cpp
+++ TivaC/catkin_ws/src/ros_tivac/chatter/chatter.cpp
@@ -19,33 +19,55 @@ adc_joystick_msg::ADC_Joystick js_msg;
 ros::Publisher adc_joystick("adc_joystick", &js_msg);
 
 
+#define GPIO_JOYSTICK_CLICK_PORT GPIO_PORTA_BASE
+#define GPIO_JOYSTICK_CLICK_PIN GPIO_PIN_5
+
+#define GPIO_JOYSTICK_X_AXIS_PORT GPIO_PORTE_BASE
+#define GPIO_JOYSTICK_X_AXIS_PIN GPIO_PIN_2
+#define ADC_CH_JOYSTICK_X_AXIS ADC_CTL_CH1
+
+#define GPIO_JOYSTICK_Y_AXIS_PORT GPIO_PORTE_BASE
+#define GPIO_JOYSTICK_Y_AXIS_PIN GPIO_PIN_1
+#define ADC_CH_JOYSTICK_Y_AXIS ADC_CTL_CH2
+
+void enableSysPeripherals(void)
+{
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+  
+  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_ADC0))
+  {
+  }
+}
+
+
 void onButtonDown(void);
 void onButtonUp(void);
 
 void onButtonDown(void) {
-    if (GPIOIntStatus(GPIO_PORTA_BASE, false) & GPIO_PIN_5) {
+    if (GPIOIntStatus(GPIO_JOYSTICK_CLICK_PORT, false) & GPIO_JOYSTICK_CLICK_PIN) {
         // PA5 was interrupt cause
         js_msg.select = true;
         adc_joystick.publish(&js_msg);
-        GPIOIntRegister(GPIO_PORTA_BASE, onButtonUp);   // Register our handler function for port A
-        GPIOIntTypeSet(GPIO_PORTA_BASE, GPIO_PIN_5,
+        GPIOIntRegister(GPIO_JOYSTICK_CLICK_PORT, onButtonUp);   // Register our handler function for port A
+        GPIOIntTypeSet(GPIO_JOYSTICK_CLICK_PORT, GPIO_JOYSTICK_CLICK_PIN,
             GPIO_RISING_EDGE);          // Configure PA5 for rising edge trigger
-        GPIOIntClear(GPIO_PORTA_BASE, GPIO_PIN_5);  // Clear interrupt flag
+        GPIOIntClear(GPIO_JOYSTICK_CLICK_PORT, GPIO_JOYSTICK_CLICK_PIN);  // Clear interrupt flag
     }
 }
 
 void onButtonUp(void) {
-    if (GPIOIntStatus(GPIO_PORTA_BASE, false) & GPIO_PIN_5) {
+    if (GPIOIntStatus(GPIO_JOYSTICK_CLICK_PORT, false) & GPIO_JOYSTICK_CLICK_PIN) {
         // PA5 was interrupt cause
         js_msg.select = false;
         adc_joystick.publish(&js_msg);
-        GPIOIntRegister(GPIO_PORTA_BASE, onButtonDown); // Register our handler function for port F
-        GPIOIntTypeSet(GPIO_PORTA_BASE, GPIO_PIN_5,
+        GPIOIntRegister(GPIO_JOYSTICK_CLICK_PORT, onButtonDown); // Register our handler function for port F
+        GPIOIntTypeSet(GPIO_JOYSTICK_CLICK_PORT, GPIO_JOYSTICK_CLICK_PIN,
             GPIO_FALLING_EDGE);         // Configure PF4 for falling edge trigger
-        GPIOIntClear(GPIO_PORTA_BASE, GPIO_PIN_5);  // Clear interrupt flag
+        GPIOIntClear(GPIO_JOYSTICK_CLICK_PORT, GPIO_JOYSTICK_CLICK_PIN);  // Clear interrupt flag
     }
 }
-
 
 void readADC(void){
 
@@ -64,6 +86,8 @@ void readADC(void){
 
 }
 
+
+
 int main(void)
 {
   // TivaC application specific code
@@ -72,29 +96,23 @@ int main(void)
   // TivaC system clock configuration. Set to 80MHz.
   MAP_SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
 
-  // Pin F4 setup
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);        // Enable port A
-  GPIOPinTypeGPIOInput(GPIO_PORTA_BASE, GPIO_PIN_5);  // Init PA5 as input
-  GPIOPadConfigSet(GPIO_PORTA_BASE, GPIO_PIN_5,
-      GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);  // Enable weak pullup resistor for PA5
+  enableSysPeripherals();
 
-  // Interrupt setup
-  GPIOIntDisable(GPIO_PORTA_BASE, GPIO_PIN_5);        // Disable interrupt for PA5 (in case it was enabled)
-  GPIOIntClear(GPIO_PORTA_BASE, GPIO_PIN_5);      // Clear pending interrupts for PA5
-  GPIOIntRegister(GPIO_PORTA_BASE, onButtonDown);     // Register our handler function for port A
-  GPIOIntTypeSet(GPIO_PORTA_BASE, GPIO_PIN_5,
-      GPIO_FALLING_EDGE);             // Configure PF4 for falling edge trigger
-  GPIOIntEnable(GPIO_PORTA_BASE, GPIO_PIN_5);     // Enable interrupt for PF4
+  //Setup Joystick click
+  GPIOPinTypeGPIOInput(GPIO_JOYSTICK_CLICK_PORT, GPIO_JOYSTICK_CLICK_PIN);
+  GPIOPadConfigSet(GPIO_JOYSTICK_CLICK_PORT, GPIO_JOYSTICK_CLICK_PIN, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);  // Enable weak pullup resistor
 
-
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
-  GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_1|GPIO_PIN_2);
+  //Joystick click Interrupt setup
+  GPIOIntDisable(GPIO_JOYSTICK_CLICK_PORT, GPIO_JOYSTICK_CLICK_PIN);        // Disable interrupt for PA5 (in case it was enabled)
+  GPIOIntClear(GPIO_JOYSTICK_CLICK_PORT, GPIO_JOYSTICK_CLICK_PIN);      // Clear pending interrupts for PA5
+  GPIOIntRegister(GPIO_JOYSTICK_CLICK_PORT, onButtonDown);     // Register our handler function for port A
+  GPIOIntTypeSet(GPIO_JOYSTICK_CLICK_PORT, GPIO_JOYSTICK_CLICK_PIN, GPIO_FALLING_EDGE);             // Configure PF4 for falling edge trigger
+  GPIOIntEnable(GPIO_JOYSTICK_CLICK_PORT, GPIO_JOYSTICK_CLICK_PIN);     // Enable interrupt for PF4
 
 
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
-  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_ADC0))
-  {
-  }
+  //Setup joystic axis pins
+  GPIOPinTypeADC(GPIO_JOYSTICK_X_AXIS_PORT, GPIO_JOYSTICK_X_AXIS_PIN);
+  GPIOPinTypeADC(GPIO_JOYSTICK_Y_AXIS_PORT, GPIO_JOYSTICK_Y_AXIS_PIN);  
 
   ADCSequenceDisable( ADC0_BASE, 0 );
   ADCSequenceDisable( ADC0_BASE, 1 );
@@ -102,10 +120,10 @@ int main(void)
   ADCSequenceDisable( ADC0_BASE, 3 );
 
   ADCSequenceConfigure(ADC0_BASE, 1, ADC_TRIGGER_PROCESSOR, 0);
-  ADCSequenceStepConfigure(ADC0_BASE, 1, 0, ADC_CTL_CH1);
-  ADCSequenceStepConfigure(ADC0_BASE, 1, 1, ADC_CTL_CH1);
-  ADCSequenceStepConfigure(ADC0_BASE, 1, 2, ADC_CTL_CH2);
-  ADCSequenceStepConfigure(ADC0_BASE,1,3, ADC_CTL_END | ADC_CTL_CH2 | ADC_CTL_IE);
+  ADCSequenceStepConfigure(ADC0_BASE, 1, 0, ADC_CH_JOYSTICK_X_AXIS);
+  ADCSequenceStepConfigure(ADC0_BASE, 1, 1, ADC_CH_JOYSTICK_X_AXIS);
+  ADCSequenceStepConfigure(ADC0_BASE, 1, 2, ADC_CH_JOYSTICK_Y_AXIS);
+  ADCSequenceStepConfigure(ADC0_BASE,1,3, ADC_CTL_END | ADC_CH_JOYSTICK_Y_AXIS | ADC_CTL_IE);
   ADCSequenceEnable(ADC0_BASE, 1);
 
   ADCIntRegister(ADC0_BASE, 1, readADC);
