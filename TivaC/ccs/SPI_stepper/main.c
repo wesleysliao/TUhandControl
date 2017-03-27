0c502dcf83fc2c15058c70d5110b82a64db01078
wesleysliao <wesleysliao@gmail.com>
2017-03-26 19:54:57 -0500

    ccs version stepper spi config complete. prelim tests show overheating problems

--- TivaC/ccs/SPI_stepper/main.c
+++ TivaC/ccs/SPI_stepper/main.c
@@ -56,6 +56,7 @@ uint16_t Reverse16(uint16_t ui16Number)
 #define GPIO_SPI_CS_STEPPER_1_PORT GPIO_PORTA_BASE
 #define GPIO_SPI_CS_STEPPER_1_PIN  GPIO_PIN_4
 
+uint32_t SPIReadByte(uint8_t address);
 
 void SPIWriteByte(uint8_t address, uint8_t data){
     GPIOPinWrite(GPIO_SPI_CS_STEPPER_1_PORT, GPIO_SPI_CS_STEPPER_1_PIN, 0); //Pull CS LOW
@@ -69,7 +70,32 @@ void SPIWriteByte(uint8_t address, uint8_t data){
     }
 
     GPIOPinWrite(GPIO_SPI_CS_STEPPER_1_PORT, GPIO_SPI_CS_STEPPER_1_PIN, 255); //Pull CS HIGH
+
+    SysCtlDelay(50);
+
+
+    SPIReadByte(address);
 }
+
+uint32_t SPIReadByte(uint8_t address){
+    GPIOPinWrite(GPIO_SPI_CS_STEPPER_1_PORT, GPIO_SPI_CS_STEPPER_1_PIN, 0); //Pull CS LOW
+
+    uint32_t readRequest = (address << 8);
+    uint32_t dataIn =0;
+
+    SSIDataPut(SSI2_BASE, readRequest);
+
+    while(SSIBusy(SSI2_BASE))
+    {
+    }
+
+    while(SSIDataGetNonBlocking(SSI2_BASE, &dataIn) != 0);
+
+    GPIOPinWrite(GPIO_SPI_CS_STEPPER_1_PORT, GPIO_SPI_CS_STEPPER_1_PIN, 255); //Pull CS HIGH
+
+    return dataIn;
+}
+
 
 void ClearStepperRegisters(void){
     WR_reg = 0;
@@ -89,31 +115,31 @@ void SetStepperCurrent(uint16_t milliamps)
  {
      // This comes from Table 13 of the AMIS-30543 datasheet.
      uint8_t code = 0;
-     if      (milliamps == 3000) { code = 0b11001; }
-     else if (milliamps == 2845) { code = 0b11000; }
-     else if (milliamps == 2700) { code = 0b10111; }
-     else if (milliamps == 2440) { code = 0b10110; }
-     else if (milliamps == 2240) { code = 0b10101; }
-     else if (milliamps == 2070) { code = 0b10100; }
-     else if (milliamps == 1850) { code = 0b10011; }
-     else if (milliamps == 1695) { code = 0b10010; }
-     else if (milliamps == 1520) { code = 0b10001; }
-     else if (milliamps == 1405) { code = 0b10000; }
-     else if (milliamps == 1260) { code = 0b01111; }
-     else if (milliamps == 1150) { code = 0b01110; }
-     else if (milliamps == 1060) { code = 0b01101; }
-     else if (milliamps ==  955) { code = 0b01100; }
-     else if (milliamps ==  870) { code = 0b01011; }
-     else if (milliamps ==  780) { code = 0b01010; }
-     else if (milliamps ==  715) { code = 0b01001; }
-     else if (milliamps ==  640) { code = 0b01000; }
-     else if (milliamps ==  585) { code = 0b00111; }
-     else if (milliamps ==  540) { code = 0b00110; }
-     else if (milliamps ==  485) { code = 0b00101; }
-     else if (milliamps ==  445) { code = 0b00100; }
-     else if (milliamps ==  395) { code = 0b00011; }
-     else if (milliamps ==  355) { code = 0b00010; }
-     else if (milliamps ==  245) { code = 0b00001; }
+     if      (milliamps <= 3000) { code = 0b11001; }
+     else if (milliamps <= 2845) { code = 0b11000; }
+     else if (milliamps <= 2700) { code = 0b10111; }
+     else if (milliamps <= 2440) { code = 0b10110; }
+     else if (milliamps <= 2240) { code = 0b10101; }
+     else if (milliamps <= 2070) { code = 0b10100; }
+     else if (milliamps <= 1850) { code = 0b10011; }
+     else if (milliamps <= 1695) { code = 0b10010; }
+     else if (milliamps <= 1520) { code = 0b10001; }
+     else if (milliamps <= 1405) { code = 0b10000; }
+     else if (milliamps <= 1260) { code = 0b01111; }
+     else if (milliamps <= 1150) { code = 0b01110; }
+     else if (milliamps <= 1060) { code = 0b01101; }
+     else if (milliamps <=  955) { code = 0b01100; }
+     else if (milliamps <=  870) { code = 0b01011; }
+     else if (milliamps <=  780) { code = 0b01010; }
+     else if (milliamps <=  715) { code = 0b01001; }
+     else if (milliamps <=  640) { code = 0b01000; }
+     else if (milliamps <=  585) { code = 0b00111; }
+     else if (milliamps <=  540) { code = 0b00110; }
+     else if (milliamps <=  485) { code = 0b00101; }
+     else if (milliamps <=  445) { code = 0b00100; }
+     else if (milliamps <=  395) { code = 0b00011; }
+     else if (milliamps <=  355) { code = 0b00010; }
+     else if (milliamps <=  245) { code = 0b00001; }
 
      CR0_reg = (CR0_reg & 0b11100000) | code;
      SPIWriteByte(CR0, CR0_reg);
@@ -133,6 +159,10 @@ void SetStepperDirection(bool forward){
 
 void StepperEnable(void){
     CR2_reg = (CR2_reg | 0b10000000);
+    SPIWriteByte(CR2, CR2_reg);
+}
+void StepperDisable(void){
+    CR2_reg = 0;
     SPIWriteByte(CR2, CR2_reg);
 }
 
@@ -181,34 +211,64 @@ int main(void)
     SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI2);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+
+    GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE,GPIO_PIN_0);
+    GPIOPadConfigSet(GPIO_PORTE_BASE,GPIO_PIN_0,GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD);
 
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE,GPIO_PIN_2);
     GPIOPadConfigSet(GPIO_PORTF_BASE,GPIO_PIN_2,GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD);
-
-    GPIOPinWrite(GPIO_SPI_CS_STEPPER_1_PORT, GPIO_SPI_CS_STEPPER_1_PIN, 255); //Pull CS HIGH
 
     GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE,GPIO_PIN_4);
     GPIOPadConfigSet(GPIO_PORTA_BASE,GPIO_PIN_4,GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD);
 
     GPIOPinConfigure(GPIO_PB4_SSI2CLK);
     GPIOPinConfigure(GPIO_PB7_SSI2TX);
-    GPIOPinTypeSSI(GPIO_PORTB_BASE,GPIO_PIN_4|GPIO_PIN_7);
+    GPIOPinConfigure(GPIO_PB6_SSI2RX);
+    GPIOPinTypeSSI(GPIO_PORTB_BASE,GPIO_PIN_4|GPIO_PIN_6|GPIO_PIN_7);
 
-    SSIConfigSetExpClk(SSI2_BASE, SysCtlClockGet(), SSI_FRF_MOTO_MODE_0, SSI_MODE_MASTER, 10000, 16);
+    SSIConfigSetExpClk(SSI2_BASE, SysCtlClockGet(), SSI_FRF_MOTO_MODE_0, SSI_MODE_MASTER, 100000, 16);
     SSIEnable(SSI2_BASE);
 
+    GPIOPinWrite(GPIO_SPI_CS_STEPPER_1_PORT, GPIO_SPI_CS_STEPPER_1_PIN, 255); //Pull CS HIGH
+
+    GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_0, 255); //Pull CLR HIGH
+    SysCtlDelay(1000);
+    GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_0, 0); //Pull CLR LOW
+
+    uint32_t buf;
+    while(SSIDataGetNonBlocking(SSI2_BASE, &buf) != 0); //clear spi fifo buffer
+
+
     ClearStepperRegisters();
-    SetStepperCurrent(1000);
-    SetStepperStepMode(STEPMODE_UNCOMP_FULL);
+    SetStepperCurrent(2800);
+    SetStepperStepMode(STEPMODE_MICRO_8);
     StepperEnable();
 
 
     while(1)
     {
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 255);
-        SysCtlDelay(100000);
+        SysCtlDelay(10000);
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
-        SysCtlDelay(100000);
+        SysCtlDelay(10000);
+
+        uint32_t sr0_stat = SPIReadByte(SR0);
+        uint32_t sr1_stat = SPIReadByte(SR1);
+        uint32_t sr2_stat = SPIReadByte(SR2);
+
+        if(((sr0_stat | sr1_stat | sr2_stat ) & 0b1111111) > 0){
+
+            StepperDisable();
+
+            uint32_t sr3_stat = SPIReadByte(SR3);
+            uint32_t sr4_stat = SPIReadByte(SR4);
+            //error detected
+            SysCtlDelay(1);
+
+            StepperEnable();
+        }
+
     }
 }
