6237b6fd5f884c6e383605158742bf4cc72aa237
Unknown <arthur-win@utulsa.edu>
2017-03-28 23:48:10 -0500

    completed GUI joystick subscribing

---
+++ TivaC/catkin_ws/src/my_qt_gui_subscriber/include/my_qt_gui_subscriber/qnode.hpp
@@ -0,0 +1,74 @@
+/**
+ * @file /include/my_qt_gui_subscriber/qnode.hpp
+ *
+ * @brief Communications central!
+ *
+ * @date February 2011
+ **/
+/*****************************************************************************
+** Ifdefs
+*****************************************************************************/
+
+#ifndef my_qt_gui_subscriber_QNODE_HPP_
+#define my_qt_gui_subscriber_QNODE_HPP_
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include <ros/ros.h>
+#include <string>
+#include <std_msgs/UInt16.h>
+#include <QThread>
+#include <QStringListModel>
+#include "adc_joystick_msg/ADC_Joystick.h"
+
+
+/*****************************************************************************
+** Namespaces
+*****************************************************************************/
+
+namespace my_qt_gui_subscriber {
+
+/*****************************************************************************
+** Class
+*****************************************************************************/
+
+class QNode : public QThread {
+    Q_OBJECT
+public:
+	QNode(int argc, char** argv );
+	virtual ~QNode();
+	bool init();
+	bool init(const std::string &master_url, const std::string &host_url);
+	void run();
+        void myCallback(const adc_joystick_msg::ADC_Joystick& message_holder);
+
+	/*********************
+	** Logging
+	**********************/
+	enum LogLevel {
+	         Debug,
+	         Info,
+	         Warn,
+	         Error,
+	         Fatal
+	 };
+
+	QStringListModel* loggingModel() { return &logging_model; }
+        void log( const LogLevel &level, int x, int y);
+
+Q_SIGNALS:
+	void loggingUpdated();
+    void rosShutdown();
+
+private:
+	int init_argc;
+	char** init_argv;
+	ros::Subscriber chatter_subscriber;
+    QStringListModel logging_model;
+};
+
+}  // namespace my_qt_gui_subscriber
+
+#endif /* my_qt_gui_subscriber_QNODE_HPP_ */
