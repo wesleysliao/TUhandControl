bbbdc7d680f4eb8359e45fa2c9ba0cbcf331a8b4
wesleysliao <wesleysliao@gmail.com>
2017-04-18 13:52:18 -0500

    Added script and launchfile to set all parameters and start needed nodes

--- TivaC/catkin_ws/start_ros_debug.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source devel/setup.bash
-
-gnome-terminal --command 'roscore'
-
-gnome-terminal --title='/adc_joystick' --command 'rostopic echo adc_joystick'
-gnome-terminal --title='/stepper_status' --command 'rostopic echo stepper_status'
-
